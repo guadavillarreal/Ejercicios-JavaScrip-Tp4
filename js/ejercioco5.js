a2e0f17bc8fd36b76e90e2b6bf4b9d2bf2dd7bda
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-11-25 19:08:56 -0300

    planteo y resolucion parcial del ejercicio 5

---
+++ js/ejercioco5.js
@@ -0,0 +1,141 @@
+/*5- Crea una clase llamada Persona que siga las siguientes 
+condiciones:
+Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer),
+ peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
+Los métodos que se debe poder utilizar  son:
+ - mostrarGeneracion: este método debe mostrar un mensaje indicando 
+    a qué generación pertenece la persona creada y cual es el rasgo 
+    característico de esta generación.Para realizar este método tener 
+    en cuenta la siguiente tabla de generaciones:
+ - esMayorDeEdad: indica si es mayor de edad, devuelve un mensaje 
+    indicando que la persona es mayor de edad.
+ - mostrarDatos: devuelve toda la información del objeto.
+    generaDNI(): genera un número aleatorio de 8 cifras.*/
+
+class Persona {
+  #edad;
+  #dni;
+  #peso;
+  #altura;
+  #nacimiento;
+  constructor(
+    nombreParam,
+    edadParam,
+    dniParam,
+    sexoParam,
+    pesoParam,
+    alturaParam,
+    nacimientoParam
+  ) {
+    this.nombre = nombreParam;
+    this.#edad = edadParam;
+    this.#dni = dniParam;
+    this.sexo = sexoParam;
+    this.#peso = pesoParam;
+    this.#altura = alturaParam;
+    this.#nacimiento = nacimientoParam;
+  }
+  get edad() {
+    return this.#edad;
+  }
+  set edad(nuevaedad) {
+    if (nuevaedad > 0) {
+      this.#edad = nuevaedad;
+    } else {
+      console.log("EL número ingresado no es valido");
+    }
+  }
+  get dni() {
+    return this.#dni;
+  }
+  set dni(nuevodni) {
+    if (nuevodni > 0) {
+      this.#dni = nuevodni;
+    } else {
+      console.log("El dni ingresa no es valido");
+    }
+  }
+  get peso() {
+    return this.#peso;
+  }
+  set peso(nuevoPeso) {
+    if (nuevoPeso > 0) {
+      this.#peso = nuevoPeso;
+    } else {
+      console.log("El peso ingresa no es valido");
+    }
+  }
+  get altura() {
+    return this.#altura;
+  }
+  set altura(nuevaaltura) {
+    if (nuevaaltura > 0) {
+      this.#altura = nuevaaltura;
+    } else {
+      console.log("La altura ingresa no es valida");
+    }
+  }
+  get nacimiento() {
+    return this.#nacimiento;
+  }
+  set nacimiento(nuevonacimiento) {
+    if (nuevonacimiento > 0) {
+      this.#nacimiento = nuevonacimiento;
+    } else {
+      console.log("El año ingresa no es valido");
+    }
+  }
+  mostrarGeneracion() {
+    let generacion = "";
+    let razgo = "";
+    if (this.nacimiento >= 1930 && this.nacimiento <= 1948) {
+      generacion = "Silent Generation";
+      razgo = "la AUSTERIDAD";
+    } else {
+      if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
+        generacion = "Baby Boom";
+        razgo = "Ambición";
+      } else {
+        if (this.nacimiento >= 1969 && this.nacimiento <= 1980) {
+          generacion = "Generación X";
+          razgo = "Obseción por el éxito";
+        } else {
+          if (this.nacimiento >= 1981 && this.nacimiento <= 1993) {
+            generacion = "Generacion Y MILLENNIALS";
+            razgo = "Frustración";
+          } else {
+            if (this.nacimiento >= 1994 && this.nacimiento <= 2010) {
+              generacion = "Generación Z";
+              razgo = "Irreverencia";
+            } else {
+              generacion = "no tenemos parametros para su generación"
+              razgo = " "
+            }
+          }
+        }
+      }
+    }
+    console.log(`La persona pertenece a la generación:${generacion}`);
+    console.log(`Su razgo caracteristico es: ${razgo}`);
+  }
+  esMayorDeEdad() {
+    if (this.edad >= 18) {
+      console.log(`La persona es mayor de edad`);
+    } else {
+      console.log(`La persona es menor de edad`);
+    }
+  }
+  mostrarDatos() {
+    console.log(`<h2>Persona: ${this.nombre}</h2>
+        <ul>
+        <li>${this.edad}</li>
+        <li>${this.dni}</li>
+        <li>${this.sexo}</li>
+        <li>${this.peso}</li>
+        <li>${this.altura}</li>
+        <li>${this.nacimiento}</li>
+        </ul>
+        `);
+  }
+}
+//me falta ver lo del dni ---    generaDNI(): genera un número aleatorio de 8 cifras.
